2747c248024f88973dc27aad40b02bde4799ca94
bernie <mail@javaner.de>
2023-09-24 08:44:26 +0200

    change: use unpkg.com

--- test/js/test.es6.js
+++ test/js/test.es6.js
@@ -1,10 +1,9 @@
 
-import * as THREE from '../../node_modules/three/build/three.module.js';
-import {Domevents, DomeventPointer} from "../../node_modules/three-domevents/dist/domevents.pack.es.js";
-import Viewport from "../../node_modules/three-viewport/dist/viewport.es.js";
+import * as THREE from 'three';
+import {Domevents, DomeventPointer} from "domevents.pack";
+import Viewport from "viewport";
 import WoodBox from "./WoodBox.js";
 import chaser from "../../src/chaser.es.js";
-import TWEEN from "../../node_modules/@tweenjs/tween.js/dist/tween.esm.js";
 
 Domevents.extend( DomeventPointer.config({emulateMouse:true}) );
 
@@ -21,7 +20,7 @@ function init() {
     VP.init();
     VP.start();
 
-    VP.loop.add( function(){ TWEEN.update(); });
+    VP.loop.add( function(){ chaser.TWEEN.update(); });
 
     VP.camera.position.z = 400;
 
@@ -53,41 +52,8 @@ function init() {
     mesh2.onClick = function(){ 
         console.log("click mesh1"); 
         rotChaser.toggle();
-    };
+    }
 
-
-
-    let mesh = new WoodBox(200, 200, 200);
-    mesh.name = "box_gross";
-
-    mesh.onClick = function( ev ){
-        console.log("mesh onclick", ev );
-        VP.scene.remove(mesh);
-        setTimeout(function(){ VP.scene.add(mesh); }, 6000);
-    };
-
-    let box = new THREE.Mesh( new THREE.BoxGeometry(20,20,20),new THREE.MeshBasicMaterial({color:"green"}) );
-    box.name = "mini_1";
-    box.position.set(100,100,100);
-
-    let box2 = new THREE.Mesh( new THREE.BoxGeometry(20,20,20), new THREE.MeshBasicMaterial({color:"red"}) );
-    box2.name = "mini_2";
-    box2.position.set( 100, -100, 100 );
-    
-    box2.onClick = function( ev ){
-        console.log("*** red box click", ev );
-        ev.stopPropagation();
-    };
-    box.onClick = function( ev ){
-        console.log("*** box click", ev );
-    };
-
-    VP.scene.add( mesh1 );
-    VP.scene.add( mesh2 );
-
-    VP.scene.add( mesh );
-
-    mesh.add(box);
-    mesh.add(box2);
+    VP.scene.add( mesh1, mesh2 );
 
 }
