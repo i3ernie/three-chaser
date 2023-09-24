185918179ed3ff96449fd11d51e79b3d3be1190d
bernie <mail@javaner.de>
2023-09-24 08:46:22 +0200

    add: new task copy

--- gulpfile.js
+++ gulpfile.js
@@ -9,3 +9,14 @@ gulp.task("buildES", build_chaserES );
 gulp.task("buildAMD", build_chaserAMD );
 
 gulp.task('default', gulp.series('buildES') );
+
+gulp.task('copy', function( ) {
+    return gulp.src([
+        "./node_modules/three/build/three.module.js",
+        "./node_modules/three/examples/jsm/controls/OrbitControls.js",
+        "./node_modules/three-viewport/dist/viewport.es.js",
+        "./node_modules/three-domevents/dist/domevents.pack.es.js"
+    ])
+    .pipe( gulp.dest('./examples/js/vendor/') );
+
+});
